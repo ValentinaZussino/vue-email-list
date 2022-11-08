a92f9b832c3d38298cf4e9039ddddf5e0d4f5793
ValentinaZussino <vzussino@protonmail.com>
2022-11-08 17:17:30 +0100

    stessa cosa ma solo con il method getMails

--- js/script.js
+++ js/script.js
@@ -12,10 +12,12 @@ const app = createApp({
     },
     methods: {
         getMails(){
+            const newList = [];
             for(let i = 0; i < 10; i++){
                 axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((elem)=>{
                     console.log(elem.data.response);
-                    this.mailsList.push(elem.data.response)
+                    newList.push(elem.data.response);
+                    if(i === 9) this.mailsList = [...newList]
                 })  
             }
             this.mailsList = [];
