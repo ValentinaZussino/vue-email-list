3f230586401db7831998d24ca997dca746af59b6
ValentinaZussino <vzussino@protonmail.com>
2022-11-08 16:49:32 +0100

    stampa 10 mail di seguito

--- js/script.js
+++ js/script.js
@@ -12,10 +12,13 @@ const app = createApp({
     },
     methods: {
         getMails(){
-            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((elem)=>{
-            console.log(elem.data.response);
-            this.mailsList.push(elem.data.response)
-            })
+            for(let i = 0; i < 10; i++){
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((elem)=>{
+                    console.log(elem.data.response);
+                    this.mailsList.push(elem.data.response)
+                })  
+            }
+            
         }
     }
 
